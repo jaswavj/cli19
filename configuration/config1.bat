46f3ce4ee43cccd33a83e69551d45b206ee77ed0
jaswavj <jaswavj@gmail.com>
2026-06-08 17:06:22 +0530

    dkdk

---
+++ configuration/config1.bat
@@ -0,0 +1,4 @@
+@echo off
+attrib +h +s "D:\APPLICATION"
+echo ✅ Billing software folder is now hidden.
+pause
---
+++ configuration/config2.bat
@@ -0,0 +1,4 @@
+@echo off
+attrib -h -s "D:\application"
+echo ✅ Billing software folder is now visible.
+pause
